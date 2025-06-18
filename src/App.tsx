63f1bd5120d08bdac4d5464daa570d94b9edfd10
Alvin <Alv.Barnes@gmail.com>
2025-06-17 22:34:51 -0400

    Lifted up state and added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import { useState } from 'react'
+import { useState, } from 'react'
 import { Projects } from './Components/Projects'
 import { Important } from './Components/Important'
 import { MyDay } from './Components/MyDay'
 import { Tasks } from './Components/Tasks'
 
-type Props = {
-    changeMainPage: (e: React.MouseEvent<HTMLDivElement>) => void
-    clickedName: string
-}
-
-
 const App = () => {
-  const [myDayTasks, setMyDayTasks] = useState([{}])
   const [importantTasks, setImportantTasks] = useState([{}])
   const [projectTasks, setProjectTasks] = useState([{}])
   const [newTasks, setNewTasks] = useState([{}])
   const [randomTasks, setRandomTasks] = useState([{}])
+  const [data, setData] = useState({})
   const [clickedName, setClickedName] = useState('')
+  const [updatedText, setUpdatedText] = useState('')
 
   const changeMainPage = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     console.log(e.target, clickedName)
     setClickedName(e.currentTarget.innerText)
 
+  }
+
+  const changeText = (textValue: string) => {
+    setUpdatedText(textValue)
+  }
+
+  const createNewDivTasks = (formData: object) => {
+    setData(formData)
+
+    console.log('WOY', data)
   }
   return (
     <>
@@ -35,18 +40,18 @@ const App = () => {
         <div onClick={changeMainPage}>
             <h3>Important</h3>
         </div>
-        <div>
+        <div onClick={changeMainPage}>
             <h3>Projects</h3>
         </div>
-        <div>
+        <div onClick={changeMainPage}>
             <h3>Tasks</h3>
         </div>
       </div>
       <div>
-        <MyDay clickedName={clickedName}  />
-        <Important />
-        <Projects />
-        <Tasks />
+        <MyDay clickedName={clickedName} createNewDivTasks={createNewDivTasks} changeText={changeText} updatedText={updatedText} />
+        <Important clickedName={clickedName} createNewDivTasks={createNewDivTasks} changeText={changeText} updatedText={updatedText} />
+        <Projects clickedName={clickedName} createNewDivTasks={createNewDivTasks} changeText={changeText} updatedText={updatedText} />
+        <Tasks clickedName={clickedName} />
         
       </div>
     </>
